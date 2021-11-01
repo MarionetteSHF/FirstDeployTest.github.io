e0a711e18f0f5fccd0a63cd9cd168b5b03739e36
MarionetteSHF <hanfu.shi2019@gmail.com>
2021-10-31 23:11:04 -0400

    1

---
+++ js/app.js
@@ -0,0 +1,31 @@
+var index = 0;
+var imgs = ["./js/images/images1.png", "./js/images/images2.png", "./js/images/images3.png",
+    "./js/images/images4.png", "./js/images/images5.png", "./js/images/images6.png"
+    , "./js/images/images7.png"
+    , "./js/images/images8.png"];
+
+var t = 800;
+var set1 = setInterval("qiehuan()", t);
+function qiehuan() {
+    document.getElementById("img").src = imgs[index];
+    t -= 40;
+    index++;
+    if (t < 0) {
+        t = 800;
+    }
+    if (index > 7) {
+        index = 0;
+
+    }
+    clearInterval(set1);
+
+    set1 = setInterval("qiehuan()", t);
+}
+// setInterval("qiehuan()", 1000);
+
+
+
+
+
+
+
